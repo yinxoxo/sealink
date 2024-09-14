01995d41b8bddb3ca64a1cb1c5ac25c6d54e672d
chris <pupu86019@gmail.com>
2024-09-15 01:56:53 +0800

    chore: update project configuration to enforce double quotes for strings

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -1,14 +1,14 @@
-import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'sealink-4b0fd.firebaseapp.com',
-  projectId: 'sealink-4b0fd',
-  storageBucket: 'sealink-4b0fd.appspot.com',
-  messagingSenderId: '372285846787',
-  appId: '1:372285846787:web:65d74de1f650b0fedee536',
-  measurementId: 'G-4WNNC6CQTN',
+  authDomain: "sealink-4b0fd.firebaseapp.com",
+  projectId: "sealink-4b0fd",
+  storageBucket: "sealink-4b0fd.appspot.com",
+  messagingSenderId: "372285846787",
+  appId: "1:372285846787:web:65d74de1f650b0fedee536",
+  measurementId: "G-4WNNC6CQTN",
 };
 
 const app = initializeApp(firebaseConfig);
