d585dc04badc4069b73f9bcaa33d0e0d7e9ebba8
chris <pupu86019@gmail.com>
2024-09-15 02:04:43 +0800

    feat: navigate to card editor and dynamically render selected template

--- src/pages/CardEditor/index.jsx
+++ src/pages/CardEditor/index.jsx
@@ -1,22 +1,38 @@
-import Card from "../../cardTemplate/Card";
+import { useParams } from "react-router-dom";
+import Card2 from "../../CardTemplate/Card2";
+import Card from "../../CardTemplate/Card";
 import EditBoard from "./EditorBoard";
 
 const CardEditor = () => {
+  const { template } = useParams();
+
+  const renderTemplate = () => {
+    switch (template) {
+      case "Card":
+        return <Card />;
+      case "Card2":
+        return <Card2 />;
+      default:
+        return <Card />;
+    }
+  };
+
   return (
     <section className="ml-64 flex h-full min-h-screen w-full overflow-y-auto border-2 border-solid border-neutral-300">
       <div className="flex flex-[7] flex-col items-center border-2 border-solid border-neutral-300">
-        <div className="mb-8 mt-12 flex h-14 w-3/4 flex-row items-center justify-between border-2 border-solid border-neutral-300">
-          <div className="text-center">你的sealink</div>
+        {/* <div className="mb-8 mt-12 flex h-14 w-3/4 flex-row items-center justify-between border-2 border-solid border-neutral-300">
+          <div className="text-center">你的SeaLink</div>
           <button className="border-2 border-solid border-neutral-300">
             複製SeaLink連結
           </button>
-        </div>
+        </div> */}
         <div className="w-[560px] flex-grow border-2 border-solid border-neutral-300">
-          <Card />
+          {renderTemplate()}
         </div>
       </div>
       <EditBoard />
     </section>
   );
 };
+
 export default CardEditor;
--- src/pages/Templates/index.jsx
+++ src/pages/Templates/index.jsx
@@ -1,8 +1,38 @@
-const Templates = () => {
+import { useNavigate } from "react-router-dom";
+import Card from "../../CardTemplate/Card"; // 模板1
+import Card2 from "../../CardTemplate/Card2"; // 模板2
+
+const Template = () => {
+  const navigate = useNavigate();
+
+  const handleTemplateClick = (template) => {
+    console.log("click", template);
+    navigate(`/dashboard/card-editor/${template}`);
+  };
+
   return (
-    <>
-      <h5>template</h5>
-    </>
+    <section className="flex flex-wrap justify-center">
+      <div
+        className="m-4 flex cursor-pointer items-center justify-center overflow-hidden rounded-lg border border-gray-300"
+        style={{ width: "300px", height: "650px" }}
+        onClick={() => handleTemplateClick("Card")}
+      >
+        <div style={{ width: "100%", height: "100%", overflow: "hidden" }}>
+          <Card />
+        </div>
+      </div>
+
+      <div
+        className="m-4 flex cursor-pointer items-center justify-center overflow-hidden rounded-lg border border-gray-300"
+        style={{ width: "300px", height: "650px" }}
+        onClick={() => handleTemplateClick("Card2")}
+      >
+        <div style={{ width: "100%", height: "100%", overflow: "hidden" }}>
+          <Card2 />
+        </div>
+      </div>
+    </section>
   );
 };
-export default Templates;
+
+export default Template;
