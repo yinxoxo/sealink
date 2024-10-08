b42e559adfe7677a140a31cbc431cb56d6eacd9c
chris <pupu86019@gmail.com>
2024-10-08 11:56:19 +0800

    feat: add card flip animation to landing page section 1

---
+++ src/pages/Home/CardFigure.jsx
@@ -0,0 +1,48 @@
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa6";
+
+export const CardContainer = () => {
+  return (
+    <div className="relative">
+      <div className="flex h-full max-h-[675px] min-h-[450px] w-[8rem] min-w-[250px] max-w-[500px] transform flex-col items-center rounded-xl bg-[#D2E722] p-6">
+        <div className="mb-4 flex flex-col items-center">
+          <img
+            src="https://plus.unsplash.com/premium_photo-1671656333460-793292581bc6?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="avatar"
+            className="mb-2 h-24 w-24 rounded-full object-cover"
+          />
+          <h1 className="text-lg font-bold text-[#264F1A]">Fresh Greens</h1>
+          <p className="text-sm text-[#264F1A]">
+            Quality fruits and vegetables
+          </p>
+        </div>
+        <div className="mb-4 flex flex-col space-y-2">
+          <button className="rounded-full bg-white px-6 py-3 text-[#264F1A]">
+            Daily Fresh Picks
+          </button>
+          <button className="rounded-full bg-white px-6 py-3 text-[#264F1A]">
+            Our Best Sellers
+          </button>
+          <button className="rounded-full bg-white px-6 py-3 text-[#264F1A]">
+            Special Offers
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const SocialLinksSection = () => {
+  return (
+    <div className="flex justify-center space-x-4 py-4">
+      <div className="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-white p-2 shadow-md hover:bg-gray-200">
+        <FaFacebook className="text-3xl text-[#061592]" />
+      </div>
+      <div className="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-white p-2 shadow-md hover:bg-gray-200">
+        <FaInstagram className="text-3xl text-[#061592]" />
+      </div>
+      <div className="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-white p-2 shadow-md hover:bg-gray-200">
+        <FaTwitter className="text-3xl text-[#061592]" />
+      </div>
+    </div>
+  );
+};
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,14 @@
+import { CardContainer, SocialLinksSection } from "./CardFigure";
+import pickleImg from "../../images/pickle.png";
+
 const Home = () => {
   return (
     <div className="font-pop min-h-svh w-full">
       {/* Main Section */}
       <section className="relative flex h-screen w-full items-center justify-center bg-[#264F1A]">
-        <div className="w-[90%]">
+        <div className="flex w-[90%]">
           <div className="w-1/2">
-            <div className="mt-20 text-left text-[72px] font-[700] leading-[1.05] tracking-[-0.02em] text-[#D2E722]">
+            <div className="mt-[10rem] text-left text-[72px] font-[700] leading-[1.05] tracking-[-0.02em] text-[#D2E722]">
               Everything you are. In one, simple link in bio.
             </div>
             <p className="mt-5 leading-[1.25] tracking-[-0.02em] text-[#D2E722]">
@@ -27,6 +30,35 @@ const Home = () => {
               <button className="rounded-full bg-[#E8C0E9] p-5 text-black">
                 Claim your Linktree
               </button>
+            </div>
+          </div>
+          <div className="w-1/2" style={{ perspective: "1000px" }}>
+            <div
+              className="rotate-animation relative mt-[10rem] flex w-full justify-center"
+              style={{ transformStyle: "preserve-3d" }}
+            >
+              <div
+                className="absolute"
+                style={{ transform: "translateZ(0px)" }}
+              >
+                <CardContainer />
+              </div>
+              <div
+                className="absolute top-[22em] mr-[150px]"
+                style={{ transform: "translateZ(30px)" }}
+              >
+                <SocialLinksSection />
+              </div>
+              <div
+                className="absolute bottom-[-7rem] ml-[180px]"
+                style={{ transform: "translateZ(-30px)" }}
+              >
+                <img
+                  src={pickleImg}
+                  alt="Pickle"
+                  className="h-auto w-[300px] object-cover"
+                />
+              </div>
             </div>
           </div>
         </div>
